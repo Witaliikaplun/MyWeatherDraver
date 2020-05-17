1feeb0bb4d03e52ec0abd9a8d06386f265309489
Witaliikaplun <vitalijkaplun@gmail.com>
2020-05-17 15:56:11 +0300

    Добавил боковое меню

--- app/src/main/java/com/example/myweatherdraver/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/example/myweatherdraver/ui/gallery/GalleryFragment.java
@@ -4,23 +4,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
-import androidx.recyclerview.widget.DividerItemDecoration;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.myweatherdraver.MainActivity;
 import com.example.myweatherdraver.R;
-import com.example.myweatherdraver.list_elements.WeatherAdapter;
-import com.example.myweatherdraver.list_elements.WeatherSource;
-
-import java.util.ArrayList;
 
 public class GalleryFragment extends Fragment {
 
@@ -28,7 +16,7 @@ public class GalleryFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
-        View root = inflater.inflate(R.layout.fragment_gallery, container, false);
+        View root = inflater.inflate(R.layout.fragment_setting, container, false);
 
         return root;
     }
