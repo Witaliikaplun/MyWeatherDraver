903b4da7d05f61a88a44f0becb94fc0b73d06f98
Witaliikaplun <vitalijkaplun@gmail.com>
2020-06-04 22:09:43 +0300

    немного оптимизировал код

--- app/src/main/java/com/example/myweatherdraver/MainActivity.java
+++ app/src/main/java/com/example/myweatherdraver/MainActivity.java
@@ -45,7 +45,6 @@ public class MainActivity extends AppCompatActivity {
     private BroadcastReceiver airplanReceiver;
     WiFiReciver wiFiReciver;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
