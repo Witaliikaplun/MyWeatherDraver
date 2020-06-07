e66e396070b7a7227344ded651874d7888cb9b44
Witaliikaplun <vitalijkaplun@gmail.com>
2020-06-07 21:56:34 +0300

    оптимизация

--- app/src/main/java/com/example/myweatherdraver/ui/map/MapsFragment.java
+++ app/src/main/java/com/example/myweatherdraver/ui/map/MapsFragment.java
@@ -31,28 +31,14 @@ import static android.content.Context.LOCATION_SERVICE;
 public class MapsFragment extends Fragment {
     private static final int PERMISSION_REQUEST_CODE = 10;
     private GoogleMap mMap;
-    private Marker currentMarker;
 
 
     private OnMapReadyCallback callback = new OnMapReadyCallback() {
 
-        /**
-         * Manipulates the map once available.
-         * This callback is triggered when the map is ready to be used.
-         * This is where we can add markers or lines, add listeners or move the camera.
-         * In this case, we just add a marker near Sydney, Australia.
-         * If Google Play services is not installed on the device, the user will be prompted to
-         * install it inside the SupportMapFragment. This method will only be triggered once the
-         * user has installed Google Play services and returned to the app.
-         */
+
         @Override
         public void onMapReady(GoogleMap googleMap) {
             mMap = googleMap;
-//            LatLng sydney = new LatLng(-34, 151);
-//            googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-//            googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-
-
         }
     };
 
@@ -144,11 +130,6 @@ public class MapsFragment extends Fragment {
                     LatLng currentPosition = new LatLng(lat, lng);
                     mMap.addMarker(new MarkerOptions().position(currentPosition));
                     mMap.moveCamera(CameraUpdateFactory.newLatLng(currentPosition));
-
-                    //mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentPosition, (float) 15));
-
-
-
                 }
 
                 @Override
