58e9497023e5f959615ff83c7c7455d600d79d70
Witaliikaplun <vitalijkaplun@gmail.com>
2020-06-22 23:26:40 +0300

    добавил контекстное меню в recicleView избранных городов

--- app/src/main/java/com/example/myweatherdraver/list_elements/CityAdapter.java
+++ app/src/main/java/com/example/myweatherdraver/list_elements/CityAdapter.java
@@ -5,6 +5,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
 import com.example.myweatherdraver.R;
 import java.util.ArrayList;
@@ -13,8 +14,12 @@ import java.util.List;
 public class CityAdapter extends RecyclerView.Adapter<CityAdapter.CityFavViewHolder> {
     List<CityFavourites> listFav;
 
-    public CityAdapter(List<CityFavourites> listFav) {
+    private Fragment fragment;
+    private int menuPosition;
+
+    public CityAdapter(List<CityFavourites> listFav, Fragment fragment) {
         this.listFav = listFav;
+        this.fragment = fragment;
     }
 
     @NonNull
@@ -25,10 +30,26 @@ public class CityAdapter extends RecyclerView.Adapter<CityAdapter.CityFavViewHol
     }
 
     @Override
-    public void onBindViewHolder(@NonNull CityFavViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull CityFavViewHolder holder, final int position) {
+        TextView textElement = holder.getTextElement();
+
         holder.cityFav.setText(listFav.get(position).getCity());
         holder.citiFavTemp.setText(listFav.get(position).getTemperature());
         holder.citiFavTime.setText(listFav.get(position).getDateAndTime());
+
+        // Определяем текущую позицию в списке
+        textElement.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                menuPosition = position;
+                return false;
+            }
+        });
+
+        // Так регистрируется контекстное меню
+        if (fragment != null){
+            fragment.registerForContextMenu(textElement);
+        }
     }
 
     @Override
@@ -37,14 +58,23 @@ public class CityAdapter extends RecyclerView.Adapter<CityAdapter.CityFavViewHol
     }
 
     public class CityFavViewHolder extends RecyclerView.ViewHolder {
+        private TextView textElement;
         TextView cityFav;
         TextView citiFavTemp;
         TextView citiFavTime;
+
         public CityFavViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            textElement = itemView.findViewById(R.id.cityFav);
+
             cityFav = (TextView) itemView.findViewById(R.id.cityFav);
             citiFavTemp = (TextView) itemView.findViewById(R.id.cityFavtemp);
             citiFavTime = (TextView) itemView.findViewById(R.id.timeFav);
+        }
+
+        public TextView getTextElement() {
+            return textElement;
         }
     }
 }
--- app/src/main/java/com/example/myweatherdraver/ui/favourites/FragmentFavourites.java
+++ app/src/main/java/com/example/myweatherdraver/ui/favourites/FragmentFavourites.java
@@ -1,7 +1,10 @@
 package com.example.myweatherdraver.ui.favourites;
 
 import android.os.Bundle;
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -29,7 +32,29 @@ public class FragmentFavourites extends Fragment {
     private void initRecycleFav(View root) {
         recyclerView = (RecyclerView) root.findViewById(R.id.recyclerViewFavourites);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext(), RecyclerView.VERTICAL, false));
-        CityAdapter cityAdapter = new CityAdapter(Singleton.getSingleton().getCityFavSourceForDB().getListCity());
+        CityAdapter cityAdapter = new CityAdapter(Singleton.getSingleton().getCityFavSourceForDB().getListCity(), this);
         recyclerView.setAdapter(cityAdapter);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater inflater = requireActivity().getMenuInflater();
+        inflater.inflate(R.menu.context_menu, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        ContextMenu.ContextMenuInfo menuInfo = item.getMenuInfo();
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.remove_context:
+                //adapter.removeItem(adapter.getMenuPosition());
+                return true;
+            case R.id.clear_context:
+                //adapter.clearItems();
+                return true;
+        }
+        return super.onContextItemSelected(item);
     }
 }
